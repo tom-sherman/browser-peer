ba975d49e8963a24df9781f75dc2d6e047cd4142
Tom Sherman <the.tomsherman@gmail.com>
2017-12-31 15:22:43 +0000

    Tape testing on Windows 🙌

---
+++ test/index.js
@@ -0,0 +1,9 @@
+// This file is needed as for some reason it isn't possible to pass a glob
+// into browserify on Windows.
+
+// Browserify this file and pass it into `tape-run` to test.
+
+require('./basic.js')
+require('./binary.js')
+require('./object-mode.js')
+require('./trickle.js')
